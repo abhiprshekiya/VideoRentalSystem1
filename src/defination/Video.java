a445a3cfd2ae669e032e26791ca68beb74fca113
Asus <abhishek.singh2_cs19@gla.ac.in>
2020-08-17 10:49:47 +0530

    add fields for Video.java

--- src/defination/Video.java
+++ src/defination/Video.java
@@ -8,4 +8,13 @@
 package defination;
 
 public class Video {
+    //create Definition class
+
+    // 1 - Always put private as the visibility modifiers for fields unless specify
+    // 2 -  Always put non static as the access modifiers for fields unless specify
+
+    // member variables
+    private String videoName;
+    private boolean checkOut;
+    private int rating;
 }
